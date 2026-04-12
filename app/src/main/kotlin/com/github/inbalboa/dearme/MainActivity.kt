c87e3f4da257d0acca24b7644a2f4a44d4c044ce
Serhiy Shliapuhin <shlyapugin@gmail.com>
2026-04-12 12:16:42 +0300

    fix: use edge-to-edge and safe drawing padding

--- app/src/main/kotlin/com/github/inbalboa/dearme/MainActivity.kt
+++ app/src/main/kotlin/com/github/inbalboa/dearme/MainActivity.kt
@@ -3,7 +3,9 @@ package com.github.inbalboa.dearme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.LaunchedEffect
@@ -20,11 +22,12 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
 
         setContent {
             DearMeTheme {
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.fillMaxSize().safeDrawingPadding(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     val preferencesRepository = PreferencesRepository(this@MainActivity)
